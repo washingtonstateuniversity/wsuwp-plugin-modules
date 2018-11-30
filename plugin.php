81641a5ff81e3b53620839c613e9755597e77a3c
Danial Bleile <danbleile@gmail.com>
2018-11-30 15:38:41 -0800

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP Toolbox
-Version: 0.0.4
+Version: 0.0.5
 Description: Plugin for allowing site creators to turn features on and off.
 Author: washingtonstateuniversity, Danial Bleile
 Author URI: https://web.wsu.edu/
